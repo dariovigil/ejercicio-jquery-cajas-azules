ec11b186f21ebddc5d27193dbb9f8b070bf57f9a
Dario Vigil <dariovigil@gmail.com>
2017-10-24 12:06:51 +0200

    Finished

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function init() {
-  console.log('hola');
+  $('#menu li:first').addClass('selected');
+  $('article:nth-child(2)').addClass('middle');
+  $('.list-item:odd').addClass('highlighted');
+  $('.list-item:last').css({border:  '1px solid #eee'});
+  $('input').focus (function() {
+    $(this).css({border: '1px solid red'});
+  });
+  $('input').blur (function() {
+    $(this).css({border: '1px solid #eee'});
+  });
 };
 $(document).ready(init);
